60cb9a42e1d815193780e70350b0d9bfe620eb8a
David Anderson <dave@natulte.net>
2011-10-14 13:10:38 -0700

    hack hack hack

--- src/gonat.googlecode.com/hg/stun/stun.go
+++ src/gonat.googlecode.com/hg/stun/stun.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"bytes"
 	"io"
+	"io/ioutil"
 )
 
 // Packet classes
@@ -22,6 +23,8 @@ const (
 	binding = 0x0001
 )
 
+const magic = 0x2112a442
+
 // mapped-address
 // username
 // message-integrity
@@ -29,6 +32,102 @@ const (
 //const stunServer = "stun.l.google.com:19302"
 //const stunServer = "stun.ekiga.net:3478"
 const stunServer = "stunserver.org:3478"
+
+type stunResponse struct {
+	mappedAddr *net.UDPAddr
+}
+
+func readResponse(response []byte, expectedTid []byte) (*stunResponse, os.Error) {
+	buf := bytes.NewBuffer(response)
+	var header struct {
+		ClassAndMethods uint16
+		Length uint16
+		Magic uint32
+		Tid [12]byte
+	}
+	if err := binary.Read(buf, binary.BigEndian, &header); err != nil {
+		return nil, err
+	}
+	if header.Magic != magic {
+		return nil, os.NewError("Bad magic in response")
+	}
+	if header.ClassAndMethods != (binding | success) {
+		if header.ClassAndMethods == (binding | error) {
+			return nil, os.NewError("STUN server returned binding error")
+		} else {
+			return nil, os.NewError("STUN server sent unexpected packet")
+		}
+	}
+	if header.Tid != expectedTid {
+		return nil, os.NewError("Wrong TID in response packet")
+	}
+	if header.Length == 0 {
+		return nil, os.NewError("STUN response was empty")
+	}
+
+	// Keep track of the number of bytes of response to use when
+	// computing the (potential) MAC, if we encounter a MAC attribute.
+	macLength := sizeof(header)
+	resp := &stunResponse{}
+
+	attrs = io.LimitReader(buf, header.Length)
+	for {
+		var attrHeader struct {
+			Type uint16
+			Length uint16
+		}
+		if err = binary.Read(attrs, binary.BigEndian, &attrHeader); err != nil {
+			if err == os.EOF {
+				break
+			}
+			return nil, err
+		}
+		attrValue := io.LimitReader(attrs, attrHeader.Length)
+		switch attrHeader.Type {
+		case 0x1:
+			var addr struct {
+				Pad byte
+				Family uint8
+				Port uint16
+			}
+			if err = binary.Read(attrValue, binary.BigEndian, &addr); err != nil {
+				return nil, err
+			}
+			ipdata, err := ioutil.ReadAll(attrValue)
+			if err != nil {
+				return nil, err
+			}
+			var expectedLen int
+			switch addr.Family {
+			case 1:
+				expectedLen = 4
+			case 2:
+				expectedLen = 16
+			default:
+				return nil, os.NewError("Unknown address family")
+			}
+			if len(ipdata) != expectedLen {
+				return nil, os.NewError("Bad IP length")
+			}
+			resp.mappedAddr = &net.UDPAddr{net.IP(ipdata).To16(), addr.Port}
+
+		case 0x8:
+		case 0x20:
+		default:
+			// Skip past the unknown attribute
+			if _, err = ioutil.ReadAll(attrValue); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// We're done parsing attributes and apparently had no errors. If
+	// we found a reflected address, we're good, else boom.
+	if resp.mappedAddr == nil {
+		return nil, os.NewError("No mapped address in STUN response")
+	}
+	return resp, nil
+}
 
 func readResponse(packet []byte) (payload []byte, os.Error) {
 	resp := bytes.NewBuffer(packet)
