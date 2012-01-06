66db5ad309d8241c37ac4b0f2eb6cde4faecd005
David Anderson <dave@natulte.net>
2012-01-05 18:43:44 -0800

    Reorganize tree. Build broken.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-ROOT=code.google.com/p/gonat
+actual=$(readlink -f $0)
+expected="$GOPATH/src/code.google.com/p/gonat/build.sh"
 
+if [[ $actual != $expected ]]; then
+    echo "Not in a dev setup, abort."
+    exit 1
+fi
+
+ROOT=code.google.com/p/gonat
 TARGETS="$ROOT/nat $ROOT/nat/stun $ROOT/nat/stun/stunclient $ROOT/nat/test"
 
-mkdir -p src/code.google.com/p/gonat
-rm src/code.google.com/p/gonat/nat
-ln -sf `pwd`/nat src/code.google.com/p/gonat/nat
-
-export GOPATH=`pwd`
 go fix $TARGETS
 go fmt $TARGETS
-go install -x $TARGETS test
+go install -x $TARGETS
