a583ae108fd9223dd6f7bc6d6a94fd2b31d2419b
David Anderson <dave@natulte.net>
2012-01-22 16:07:31 -0600

    Rewrite the test app so that it implements a single end of a NATed connection.
    This enables testing across real NATs, assuming I have a way to ssh
    into a machine behind them.

--- test/test.go
+++ test/test.go
@@ -1,33 +1,25 @@
 package main
 
 import (
+	"code.google.com/p/bouncer"
 	"code.google.com/p/nat"
 	"fmt"
-	"net"
 )
 
-var ch = make(chan bool)
+func main() {
+	sideband, initiator, err := bouncer.Dial("http://natulte.net:4242/bounce/lolol")
 
-func RunNat(sideband net.Conn, initiator bool) {
-	defer func() { ch <- true }()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	conn, err := nat.Connect(sideband, initiator)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Got a NAT connection! %s to %s\n", conn.LocalAddr(), conn.RemoteAddr())
-}
 
-func main() {
-	fmt.Println("Pipe")
-	a, b := net.Pipe()
-
-	fmt.Println("Go 1")
-	go RunNat(a, true)
-	fmt.Println("Go 2")
-	go RunNat(b, false)
-	fmt.Println("ch 1")
-	<-ch
-	fmt.Println("ch 2")
-	<-ch
+	fmt.Printf("Got a nat-busted connection! %s to %s", conn.LocalAddr(), conn.RemoteAddr())
+	conn.Close()
 }
